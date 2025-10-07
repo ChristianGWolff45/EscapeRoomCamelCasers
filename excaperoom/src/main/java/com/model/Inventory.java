f0f5ad76850195991007701b12f813384cccfd2e
ChristianGWolff45 <Christianwolff@gmail.com>
2025-10-07 14:20:52 -0400

    inventory class

--- excaperoom/src/main/java/com/model/Inventory.java
+++ excaperoom/src/main/java/com/model/Inventory.java
@@ -6,7 +6,16 @@ public class Inventory {
     private ArrayList<Clue> clues;
     private ArrayList<Hint> hints;
 
-    public Inventory() {
-        
+    public Inventory(ArrayList<Clue> clues, ArrayList<Hint> hints) {
+        this.clues = clues;
+        this.hints = hints;
+    }
+    
+    public ArrayList<Clue> getClues(){
+        return clues;
+    }
+
+    public ArrayList<Hint> getHints(){
+        return hints;
     }
 }
