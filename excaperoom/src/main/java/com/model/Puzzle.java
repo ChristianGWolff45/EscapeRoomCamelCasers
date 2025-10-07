2f57de95ea9637fd5a2c956d39e931a9c208e93c
CSE-ParkerMcClure <parker.m.mcc@gmail.com>
2025-10-07 14:12:15 -0400

    fixed #16

--- excaperoom/src/main/java/com/model/Puzzle.java
+++ excaperoom/src/main/java/com/model/Puzzle.java
@@ -8,19 +8,57 @@ public class Puzzle {
     private String answer;
     private boolean completed;
 
-    public Puzzle() {
-
+    public Puzzle(ArrayList<Clue> clues, ArrayList<Hint> hints, String answer) {
+        this.clues = clues;
+        this.hints = hints;
+        this.answer = answer;
+        this.completed = false;
     }
 
+    /**
+     * check if answer is correct, if so, mark puzzle as completed
+     * @param answer
+     */
     public void enterAnswer(String answer) {
-
+        if (this.answer.equals(answer)) {
+            this.completed = true;
+        }
     }
 
-    public void completePuzzle() {
-
+    /**
+     * get state of puzzle
+     * @return
+     */
+    public boolean isCompleted() {
+        return this.completed;
     }
 
+    /**
+     * use a skip and mark puzzle as completed
+     */
     public void useSkip() {
-        
+        this.completed = true;
+        // Certificate.useSkip();
+    }
+
+    public ArrayList<Hint> getHints() {
+        return hints;
+    }
+
+    public ArrayList<Clue> getClues() {
+        return clues;
+    }
+
+    /**
+     * use a hint and display it (if available)
+     */
+    public void useHint() {
+        if (hints.size() > 0) {
+            Hint hint = hints.remove(0);
+            // Certificate.useHint();
+            hint.displayHint();
+        } else {
+            System.out.println("No more hints available.");
+        }
     }
 }
