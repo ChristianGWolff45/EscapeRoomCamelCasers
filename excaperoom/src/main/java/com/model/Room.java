d980da2ab492112170e8e73940389f5dffb93371
ChristianGWolff45 <Christianwolff@gmail.com>
2025-10-07 14:04:26 -0400

    room class

--- excaperoom/src/main/java/com/model/Room.java
+++ excaperoom/src/main/java/com/model/Room.java
@@ -7,29 +7,49 @@ public class Room {
     private ArrayList<Puzzle> puzzles;
     private ArrayList<Room> nextRooms;
     private boolean unlocked;
-    private Orientation orientation;
+    private boolean isExit;
 
-    public Room(String roomName, ArrayList<Puzzle> puzzles, ArrayList<Room> nextRooms, boolean unlocked, Orientation orientation) {
+    public Room(String roomName, ArrayList<Puzzle> puzzles, ArrayList<Room> nextRooms, boolean unlocked, boolean isExit) {
         this.roomName = roomName;
         this.puzzles = puzzles;
         this.nextRooms = nextRooms;
         this.unlocked = unlocked;
-        this.orientation = orientation;
+        this.isExit = isExit;
     }
 
-    public void goNextRoom(String s) {
-        
+    public String getName(){
+        return roomName;
+    }
+
+    public ArrayList<Puzzle> getPuzzles(){
+        return puzzles;
+    }
+
+
+    public Room goNextRoom(String s) {
+        for(Room neighbor : nextRooms){
+            if(neighbor.getName().equals(s)){
+                if(neighbor.unlocked){
+                    return neighbor;
+                }
+            }
+        }
+        return null;
     }
 
     public boolean isUnlocked() {
-        return true;
+        return unlocked;
     }
 
     public void unlock(String s) {
-
+        for(Room neighbor : nextRooms){
+            if(neighbor.getName().equals(s)){
+                neighbor.unlocked = true;
+            }
+        }
     }
 
-    public void isLastRoom() {
-        
+    public boolean isExit() {
+        return isExit();
     }
 }
