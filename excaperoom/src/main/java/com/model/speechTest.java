94443b9b5ed1f0f6e5ec6bea1fa715af746be2be
CSE-ParkerMcClure <parker.m.mcc@gmail.com>
2025-10-21 13:59:51 -0400

    added text to speech

---
+++ excaperoom/src/main/java/com/model/speechTest.java
@@ -0,0 +1,10 @@
+package com.model;
+
+import com.speech.Speak;
+
+public class speechTest {
+    public static void main(String[] args) {
+        String message = "Hey twin, how are you today twin?";
+        Speak.speak(message);
+    }
+}
