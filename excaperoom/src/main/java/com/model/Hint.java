d8c1751f5719a7900c78b29c6f33260a00106c70
CSE-ParkerMcClure <parker.m.mcc@gmail.com>
2025-10-23 14:04:20 -0400

    fixed #28

--- excaperoom/src/main/java/com/model/Hint.java
+++ excaperoom/src/main/java/com/model/Hint.java
@@ -1,18 +1,31 @@
 package com.model;
 
+import com.speech.Speak;
+
 public class Hint {
     private String tip;
     private String id;
 
+    /**
+     * Constructor for Hint
+     * @param tip
+     * @param id
+     */
     public Hint(String tip, String id) {
         this.tip = tip;
         this.id = id;
     }
 
+    /**
+     * Use TTS to speak hint tip
+     */
     public void hearHint() {
-
+        Speak.speak(tip);
     }
 
+    /**
+     * Display hint tip
+     */
     public void displayHint() {
         System.out.println(id + ": " + tip);
     }
