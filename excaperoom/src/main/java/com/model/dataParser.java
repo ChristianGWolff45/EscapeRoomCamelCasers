f61f90b1354e8097381aaacc306eeaeb70544ac7
ChristianGWolff45 <Christianwolff@gmail.com>
2025-10-24 13:12:52 -0400

    added dataParser file

---
+++ excaperoom/src/main/java/com/model/dataParser.java
@@ -0,0 +1,15 @@
+package com.model;
+
+public class dataParser {
+    public static Puzzle findPuzzle(String puzzleID){
+        Puzzle target = null;
+        
+    }
+    public static Clue findClue(String puzzleID){
+        
+    }
+    public static Hint findHint(String puzzleID){
+        
+    }
+
+}
