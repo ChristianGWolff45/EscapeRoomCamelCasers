b117adf74b8f2cc988811ac19640d778ef25952c
ChristianGWolff45 <Christianwolff@gmail.com>
2025-10-26 22:17:49 -0400

    added hard test

--- excaperoom/src/main/java/com/model/EscapeRoomUI.java
+++ excaperoom/src/main/java/com/model/EscapeRoomUI.java
@@ -31,5 +31,50 @@ public class EscapeRoomUI {
                 goBackToSelection = playMenu.run();
             }
         }
+        playGame();
     }
+    private void playGame() {
+        // Call manu that leads to options to start a new game or load previous save
+        EscapeRoom escapeRoom = new EscapeRoom();
+        escapeRoom.signUp("LRIVERS", "Leni", "Rivers", "password");
+
+        escapeRoom.signUp("LRIVERS45", "Leni", "Rivers", "password");
+        escapeRoom.login("LRIVERS45", "password");
+
+        System.out.println("Pick a game to play");
+        escapeRoom.printGames();
+
+        escapeRoom.pickGame("Escape from Swearingen");
+        escapeRoom.playStory();
+
+        escapeRoom.enterGame();
+
+        escapeRoom.pickUpClue("wordlePuzzle_Clue");
+        escapeRoom.pickUpClue("multipleChoice_Clue1");
+        escapeRoom.pickUpClue("multipleChoice_Clue2");
+
+        escapeRoom.useHint("directionalPuzzle_Hint1");
+
+        escapeRoom.useHint("wordlePuzzle_Hint");
+
+        escapeRoom.solvePuzzle("wordlePuzzle", "CRANE");
+        escapeRoom.solvePuzzle("directionalPuzzle", new Direction[] { Direction.RIGHT, Direction.DOWN, Direction.UP, Direction.LEFT });
+        escapeRoom.skipPuzzle("multiplechoice");
+
+        escapeRoom.logout();
+
+        escapeRoom.login("LRIVERS45", "password");
+        escapeRoom.pickGame("Escape from Swearingen");
+        escapeRoom.loadProgress();
+        escapeRoom.enterGame();
+
+        escapeRoom.goNextRoom("East Hallway");
+        escapeRoom.useHint("directionalPuzzle_Hint2");
+        escapeRoom.skipPuzzle("multiplechoice2");
+
+
+    }
+
+    
+
 }
