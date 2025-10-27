e18e2f5df056ccd57d6449ed43e3a360d1f375f2
ChristianGWolff45 <Christianwolff@gmail.com>
2025-10-26 22:20:50 -0400

    readded hardcode

--- excaperoom/src/main/java/com/model/EscapeRoomUI.java
+++ excaperoom/src/main/java/com/model/EscapeRoomUI.java
@@ -1,79 +1,188 @@
 package com.model;
 
+import java.util.Scanner;
+
 public class EscapeRoomUI {
-    public static void main(String[] args) {
-        EscapeRoom escapeRoom = new EscapeRoom();
+    // public static void main(String[] args) {
+    //     EscapeRoom escapeRoom = new EscapeRoom();
+
+    //     while (true) {
+    //         // Step 1: Main menu (login/signup)
+    //         MainMenu mainMenu = new MainMenu(escapeRoom);
+    //         mainMenu.run();
+
+    //         // Step 2: Game selection menu
+    //         GameSelectionMenu gameSelection = new GameSelectionMenu(escapeRoom);
+    //         String selectedGame = gameSelection.run();
+
+    //         if (selectedGame == null) {
+    //             continue; // restart loop from MainMenu
+    //         }
+
+    //         // Step 3: Play menu with selected game
+    //         PlayMenu playMenu = new PlayMenu(escapeRoom, selectedGame);
+    //         boolean goBackToSelection = playMenu.run();
+
+    //         while (goBackToSelection) {
+    //             selectedGame = gameSelection.run();
+
+    //             if (selectedGame == null) {
+    //                 break;
+    //             }
+    //             playMenu = new PlayMenu(escapeRoom, selectedGame);
+    //             goBackToSelection = playMenu.run();
+    //         }
+    //     }
+    //     playGame();
+    // }
+
+
+
+    private String[] mainMenuOptions = { "Play", "Create Account", "Login", "Logout", "Quit" };
+    private Scanner scanner;
+    private UserList userList;
+
+    EscapeRoomUI() {
+        scanner = new Scanner(System.in);
+        userList = UserList.getInstance();
+    }
+
+    public void run() {
 
         while (true) {
-            // Step 1: Main menu (login/signup)
-            MainMenu mainMenu = new MainMenu(escapeRoom);
-            mainMenu.run();
+            displayMainMenu();
 
-            // Step 2: Game selection menu
-            GameSelectionMenu gameSelection = new GameSelectionMenu(escapeRoom);
-            String selectedGame = gameSelection.run();
+            int userCommand = getUserCommand(mainMenuOptions.length);
 
-            if (selectedGame == null) {
-                continue; // restart loop from MainMenu
+            if (userCommand == -1) {
+                System.out.println("Not a valid command");
+                continue;
             }
 
-            // Step 3: Play menu with selected game
-            PlayMenu playMenu = new PlayMenu(escapeRoom, selectedGame);
-            boolean goBackToSelection = playMenu.run();
-
-            while (goBackToSelection) {
-                selectedGame = gameSelection.run();
-
-                if (selectedGame == null) {
-                    break;
+            if (userCommand == mainMenuOptions.length - 1) {
+                if (userList.isLoggedIn()) {
+                    userList.logout();
+                    System.out.println("\nYou have been logged out\n \nExiting...");
                 }
-                playMenu = new PlayMenu(escapeRoom, selectedGame);
-                goBackToSelection = playMenu.run();
+                break;
+            }
+
+            switch (userCommand) {
+                case (0):
+                    playGame();
+                    break;
+                case (1):
+                    createAccount();
+                    break;
+                case (2):
+                    login();
+                    break;
+                case (3):
+                    logout();
+                    break;
             }
         }
-        playGame();
+        System.out.println("Exiting...");
     }
+
+    private void displayMainMenu() {
+        System.out.println("***************Escape from Swearingen**************");
+        for (int i = 0; i < mainMenuOptions.length; i++) {
+            System.out.println((i + 1) + ". " + mainMenuOptions[i]);
+        }
+        System.out.println("\n");
+    }
+
+    private int getUserCommand(int numCommands) {
+        System.out.println("What would you like to do?");
+
+        String input = scanner.nextLine();
+        int command = Integer.parseInt(input) - 1;
+
+        if (command >= 0 && command <= numCommands - 1)
+            return command;
+        return -1;
+    }
+
     private void playGame() {
         // Call manu that leads to options to start a new game or load previous save
         EscapeRoom escapeRoom = new EscapeRoom();
-        escapeRoom.signUp("LRIVERS", "Leni", "Rivers", "password");
+        // escapeRoom.signUp("LRIVERS", "Leni", "Rivers", "password");
 
-        escapeRoom.signUp("LRIVERS45", "Leni", "Rivers", "password");
-        escapeRoom.login("LRIVERS45", "password");
+        // escapeRoom.signUp("LRIVERS45", "Leni", "Rivers", "password");
+        // escapeRoom.login("LRIVERS45", "password");
 
-        System.out.println("Pick a game to play");
-        escapeRoom.printGames();
+        // System.out.println("Pick a game to play");
+        // escapeRoom.printGames();
 
-        escapeRoom.pickGame("Escape from Swearingen");
-        escapeRoom.playStory();
+        // escapeRoom.pickGame("Escape from Swearingen");
+        // escapeRoom.playStory();
 
-        escapeRoom.enterGame();
+        // escapeRoom.enterGame();
 
-        escapeRoom.pickUpClue("wordlePuzzle_Clue");
-        escapeRoom.pickUpClue("multipleChoice_Clue1");
-        escapeRoom.pickUpClue("multipleChoice_Clue2");
+        // escapeRoom.pickUpClue("wordlePuzzle_Clue");
+        // escapeRoom.pickUpClue("multipleChoice_Clue1");
+        // escapeRoom.pickUpClue("multipleChoice_Clue2");
 
-        escapeRoom.useHint("directionalPuzzle_Hint1");
+        // escapeRoom.useHint("directionalPuzzle_Hint1");
 
-        escapeRoom.useHint("wordlePuzzle_Hint");
+        // escapeRoom.useHint("wordlePuzzle_Hint");
 
-        escapeRoom.solvePuzzle("wordlePuzzle", "CRANE");
-        escapeRoom.solvePuzzle("directionalPuzzle", new Direction[] { Direction.RIGHT, Direction.DOWN, Direction.UP, Direction.LEFT });
-        escapeRoom.skipPuzzle("multiplechoice");
+        // escapeRoom.solvePuzzle("wordlePuzzle", "CRANE");
+        // escapeRoom.solvePuzzle("directionalPuzzle", new Direction[] { Direction.RIGHT, Direction.DOWN, Direction.UP, Direction.LEFT });
+        // escapeRoom.skipPuzzle("multiplechoice");
 
-        escapeRoom.logout();
+        // escapeRoom.logout();
 
         escapeRoom.login("LRIVERS45", "password");
         escapeRoom.pickGame("Escape from Swearingen");
         escapeRoom.loadProgress();
         escapeRoom.enterGame();
-
+        
         escapeRoom.goNextRoom("East Hallway");
         escapeRoom.useHint("directionalPuzzle_Hint2");
         escapeRoom.skipPuzzle("multiplechoice2");
 
 
     }
+
+    private void createAccount() {
+        String username = getField("Username");
+        String firstName = getField("First Name");
+        String lastName = getField("Last Name");
+        String password = getField("Password");
+        userList.addUser(username, firstName, lastName, password);
+    }
+
+    private void login() {
+        if (userList.isLoggedIn()) {
+            System.out.println("\nYou are already logged in as " + userList.getCurrentUser().getUsername() + "\n");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        String username = getField("Username");
+        String password = getField("Password");
+        userList.loginUser(username, password);
+    }
+
+    private void logout() {
+        userList.logout();
+    }
+
+    private String getField(String prompt) {
+        System.out.println(prompt + ": ");
+        return scanner.nextLine();
+    }
+
+    public static void main(String[] args) {
+        EscapeRoomUI escapeRoomInterface = new EscapeRoomUI();
+        escapeRoomInterface.run();
+    }
+}
 
     
 
