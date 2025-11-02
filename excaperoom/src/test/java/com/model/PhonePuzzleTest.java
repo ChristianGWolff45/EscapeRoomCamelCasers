655e4c6802f7a0ed629b59730470e432d404303e
ChristianGWolff45 <Christianwolff@gmail.com>
2025-11-02 14:51:56 -0500

    tested PhonePuzzle

--- excaperoom/src/test/java/com/model/PhonePuzzleTest.java
+++ excaperoom/src/test/java/com/model/PhonePuzzleTest.java
@@ -1,5 +1,41 @@
 package com.model;
 
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
 public class PhonePuzzleTest {
-    
+    @Test
+    public void testEnterGuess_wrongGuess(){
+        PhonePuzzle p = new PhonePuzzle(null, null, "answer", false, "puzzleID");
+        String isCorrect = p.enterGuess("wrong");
+        assertTrue("isCorrect should evaluate to Incorrect, try again", isCorrect.equals("Incorrect, try again."));
+    }
+
+    @Test
+    public void testEnterGuess_writeGuess(){
+        PhonePuzzle p = new PhonePuzzle(null, null, "answer", false, "puzzleID");
+        String isCorrect = p.enterGuess("answer");
+        assertTrue("isCorrect should evaluate to Correct", isCorrect.equals("Correct!"));
+    }
+
+    @Test
+    public void testEnterGuess_differentCases(){
+        PhonePuzzle p = new PhonePuzzle(null, null, "aNsweR", false, "puzzleID");
+        String isCorrect = p.enterGuess("AnSWEr");
+        assertTrue("isCorrect should evaluate to Correct", isCorrect.equals("Correct!"));
+    }
+
+    @Test
+    public void testPuzzleCompleted_CorrectGuess(){
+        PhonePuzzle p = new PhonePuzzle(null, null, "aNsweR", false, "puzzleID");
+        p.enterGuess("AnSWEr");
+        assertTrue("puzzle should be completed", p.isCompleted());
+    }
+    @Test
+    public void testPuzzleCompleted_wrongGuess(){
+        PhonePuzzle p = new PhonePuzzle(null, null, "aNsweR", false, "puzzleID");
+        p.enterGuess("wrong");
+        assertTrue("puzzle should not be completed", !p.isCompleted());
+    }
+
 }
