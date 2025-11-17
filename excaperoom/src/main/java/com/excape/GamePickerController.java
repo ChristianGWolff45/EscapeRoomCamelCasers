5f4eab20ee223535e7938ee1e1420d1644d6b052
ChristianGWolff45 <Christianwolff@gmail.com>
2025-11-16 19:50:12 -0500

    removed controller code

---
+++ excaperoom/src/main/java/com/excape/GamePickerController.java
@@ -0,0 +1,10 @@
+package com.excape; // <- must match your fx:controller package
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.TilePane;
+
+public class GamePickerController {
+
+
+}
