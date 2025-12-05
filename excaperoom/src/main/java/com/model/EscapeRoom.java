d7d4291942f5788fc4136b35bb83546b07412e4e
nclester <nickclester@gmail.com>
2025-12-05 16:59:57 -0500

    fixed facade not saving user data

--- excaperoom/src/main/java/com/model/EscapeRoom.java
+++ excaperoom/src/main/java/com/model/EscapeRoom.java
@@ -14,18 +14,22 @@ public class EscapeRoom {
 
     }
 
-    // User Methods
     public boolean signUp(String username, String firstName, String lastName, String password) {
         boolean added = userList.addUser(username, firstName, lastName, password);
+
+        if (added) {
+            DataWriter.saveUser(); // Save after successful sign up
+        }
+
         return added;
     }
 
     public boolean login(String username, String password) {
-       boolean loggedIn = userList.loginUser(username, password);
-       return loggedIn;
+        boolean loggedIn = userList.loginUser(username, password);
+        return loggedIn;
     }
 
-    public boolean isLoggedIn(){
+    public boolean isLoggedIn() {
         return userList.getCurrentUser() != null;
     }
 
@@ -47,8 +51,9 @@ public class EscapeRoom {
         currentGame.playStory();
         gameList.enterGame();
     }
+
     public void startGame(Room room) {
-   //     gameList.setCurrentGame(gameList.newGame(userList.getCurrentUser(), room));
+        // gameList.setCurrentGame(gameList.newGame(userList.getCurrentUser(), room));
     }
 
     public void endCurrentGame() {
@@ -56,13 +61,14 @@ public class EscapeRoom {
     }
 
     public void loadGame() {
-        
+
     }
+
     /**
      * prints out all games
      */
     public void printGames() {
-        for(Game game : games){
+        for (Game game : games) {
             System.out.println(game.getName());
         }
     }
@@ -70,133 +76,149 @@ public class EscapeRoom {
     public ArrayList<Game> getAllGames() {
         return games;
     }
+
     /**
      * 
      * @param name sets current game to game of the same name
      */
-    public void pickGame(String name){
+    public void pickGame(String name) {
         gameList.setCurrentGame(name);
         userList.getCurrentUser().loadGame();
     }
+
     /**
      * loads user progress then displays it
      */
-    public void loadProgress(){
+    public void loadProgress() {
         userList.getCurrentUser().getProgress().displayProgress();
     }
+
     /**
      * hear the plot to the game
      */
-    public void playStory(){
+    public void playStory() {
         gameList.getCurrentGame().playStory();
     }
+
     /**
      * loads starting room
      */
-    public void enterGame(){
+    public void enterGame() {
         gameList.enterGame();
     }
+
     /**
      * 
      * @param clueID adds clue to user inventory
-     * then displays it
+     *               then displays it
      */
-    public void pickUpClue(String clueID){
+    public void pickUpClue(String clueID) {
         userList.getCurrentUser().addClue(clueID);
         displayClue(clueID);
     }
+
     /**
      * 
      * @param clueID displays clue
      */
 
-    public void displayClue(String clueID){
+    public void displayClue(String clueID) {
         gameList.findClue(clueID).displayClue();
     }
 
-    public void hearHint(String hintID){
+    public void hearHint(String hintID) {
         gameList.findHint(hintID).hearHint();
     }
+
     /**
      * 
      * @param hintID uses hint for user
-     * then displays it
+     *               then displays it
      */
-    public String useHint(String hintID){
+    public String useHint(String hintID) {
         userList.getCurrentUser().useHint(hintID);
+        DataWriter.saveUser(); // Save progress
         return displayHint(hintID);
     }
+
     /**
      * 
      * @param hintID displays hint
      */
-    public String displayHint(String hintID){
+    public String displayHint(String hintID) {
         return gameList.findHint(hintID).displayHint();
     }
+
     /**
      * @param puzzleID enter a puzzleID to solve
-     * @param answer enter a string answer
-     * checks if answer is answer for the puzzle
-     * if so attempts to unlock neighboring doors
-     * if last puzzle endsgame
+     * @param answer   enter a string answer
+     *                 checks if answer is answer for the puzzle
+     *                 if so attempts to unlock neighboring doors
+     *                 if last puzzle endsgame
      */
-    public void solvePuzzle(String puzzleID, String answer){
+    public void solvePuzzle(String puzzleID, String answer) {
         gameList.findPuzzle(puzzleID).enterAnswer(answer);
-        if(gameList.findPuzzle(puzzleID).isCompleted()){
+        if (gameList.findPuzzle(puzzleID).isCompleted()) {
             System.out.println("Puzzle completed");
             userList.getCurrentUser().getProgress().completePuzzle(puzzleID);
-            if(gameList.getCurrentGame().getCurrentRoom().unlockNeighbors() && gameList.getCurrentGame().getCurrentRoom().isExit()){
+            DataWriter.saveUser(); // Save progress
+            if (gameList.getCurrentGame().getCurrentRoom().unlockNeighbors()
+                    && gameList.getCurrentGame().getCurrentRoom().isExit()) {
+                endGame();
+            }
+        }
+    }
+
+    /**
+     * @param puzzleID enter a puzzleID to solve
+     * @param answer   enter an array of directions
+     *                 checks if answer is answer for the puzzle
+     *                 if so attempts to unlock neighboring doors
+     *                 if last puzzle endsgame
+     */
+
+    public void solvePuzzle(String puzzleID, Direction[] answer) {
+        gameList.findPuzzle(puzzleID).enterAnswer(answer);
+        if (gameList.findPuzzle(puzzleID).isCompleted()) {
+            System.out.println("Puzzle completed");
+            userList.getCurrentUser().getProgress().completePuzzle(puzzleID);
+            if (gameList.getCurrentGame().getCurrentRoom().unlockNeighbors()
+                    && gameList.getCurrentGame().getCurrentRoom().isExit()) {
                 endGame();
             }
 
         }
-       
     }
-    /**
-     * @param puzzleID enter a puzzleID to solve
-     * @param answer enter an array of directions
-     * checks if answer is answer for the puzzle
-     * if so attempts to unlock neighboring doors
-     * if last puzzle endsgame
-     */
 
-    public void solvePuzzle(String puzzleID, Direction[] answer){
-        gameList.findPuzzle(puzzleID).enterAnswer(answer);
-        if(gameList.findPuzzle(puzzleID).isCompleted()){
-            System.out.println("Puzzle completed");
-            userList.getCurrentUser().getProgress().completePuzzle(puzzleID);
-            if(gameList.getCurrentGame().getCurrentRoom().unlockNeighbors() && gameList.getCurrentGame().getCurrentRoom().isExit()){
-                endGame();
-            }
-
-        }
-    }
     /**
      * @param puzzleID enter a puzzleID to skip
-     * skips puzzle for current user attempts to unlock neighboring doors
-     * if last puzzle endsgame
+     *                 skips puzzle for current user attempts to unlock neighboring
+     *                 doors
+     *                 if last puzzle endsgame
      */
-    public void skipPuzzle(String puzzleID){
+    public void skipPuzzle(String puzzleID) {
         gameList.findPuzzle(puzzleID).skipPuzzle();
-        if(gameList.findPuzzle(puzzleID).isCompleted()){
+        if (gameList.findPuzzle(puzzleID).isCompleted()) {
             System.out.println("Puzzle completed");
             userList.getCurrentUser().getProgress().completePuzzle(puzzleID);
             userList.getCurrentUser().getProgress().addSkip(puzzleID);
             userList.getCurrentUser().useSkip();
-            if(gameList.getCurrentGame().getCurrentRoom().unlockNeighbors() && gameList.getCurrentGame().getCurrentRoom().isExit()){
+            DataWriter.saveUser(); // Save progress
+            if (gameList.getCurrentGame().getCurrentRoom().unlockNeighbors()
+                    && gameList.getCurrentGame().getCurrentRoom().isExit()) {
                 endGame();
             }
         }
     }
 
-    public void goNextRoom(String roomID){
+    public void goNextRoom(String roomID) {
         gameList.getCurrentGame().getCurrentRoom().unlockNeighbors();
         gameList.getCurrentGame().goNextRoom(roomID);
         System.out.println(gameList.getCurrentGame().getCurrentRoom().getName());
         gameList.getCurrentGame().getCurrentRoom().printRoom();
     }
 
-    public void endGame(){
+    public void endGame() {
         System.out.println("You escaped");
         userList.saveUsers();
         userList.printAllUserScores();
@@ -204,11 +226,11 @@ public class EscapeRoom {
 
     }
 
-    public boolean puzzleUnlocked(String puzzleID){
+    public boolean puzzleUnlocked(String puzzleID) {
         return gameList.findPuzzle(puzzleID).isCompleted();
     }
 
-    public String[][] wordleResponse(String puzzleID, String guess){
+    public String[][] wordleResponse(String puzzleID, String guess) {
         return gameList.findPuzzle(puzzleID).guessAnswer(guess);
     }
 
